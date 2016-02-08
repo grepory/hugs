aaad63154c9965a283ca0850f33d99525de54666
Dan Compton <dan@opsee.com>
2016-02-08 12:50:51 -0500

    Delete existing notif before inserting new one
    Fix tests.
    
    fix one other test jfc
    
    Delete all notifications by checkID prior to replacing them with what's sent by emissary
    
    Remove notifications prior to adding them
    
    Add new custy when we update a notification
    
    Don't delete notification prior to adding

--- src/github.com/opsee/hugs/store/postgres_test.go
+++ src/github.com/opsee/hugs/store/postgres_test.go
@@ -41,7 +41,7 @@ func NewStoreTest() *StoreTest {
 				ID:         0,
 				CustomerID: "5963d7bc-6ba2-11e5-8603-6ba085b2f5b5",
 				UserID:     13,
-				CheckID:    "00000",
+				CheckID:    "00001",
 				Value:      "off",
 				Type:       "slack_bot",
 			},
@@ -49,7 +49,7 @@ func NewStoreTest() *StoreTest {
 				ID:         1,
 				CustomerID: "5963d7bc-6ba2-11e5-8603-6ba085b2f5b5",
 				UserID:     13,
-				CheckID:    "00000",
+				CheckID:    "00001",
 				Value:      "you",
 				Type:       "email",
 			},
@@ -68,23 +68,26 @@ func NewStoreTest() *StoreTest {
 var Common = NewStoreTest()
 
 func TestStorePutNotifications(t *testing.T) {
-	for i, _ := range Common.Notifications {
-		log.Info("TestStorePutNotifications: Adding Common.Notifications[", i, "] To Store.")
-		if err := Common.DBStore.PutNotification(Common.User, Common.Notifications[i]); err != nil {
-			log.Error(err)
-			t.FailNow()
-		}
+	Common.DBStore.DeleteNotificationsByUser(Common.User)
+	log.Info("TestStorePutNotifications: Adding ", len(Common.Notifications), " To Store.")
+	if err := Common.DBStore.PutNotifications(Common.User, Common.Notifications); err != nil {
+		log.Error(err)
+		t.FailNow()
 	}
 	log.Info("TestStorePutNotifications: PASS.")
 }
 
 func TestStoreGetNotifications(t *testing.T) {
 	log.Info("TestStoreGetNotifications: Getting Common.Notifications from store")
-	if notifications, err := Common.DBStore.GetNotifications(Common.User); err != nil {
+	notifications, err := Common.DBStore.GetNotifications(Common.User)
+
+	if err != nil {
 		log.Error(err)
 		t.FailNow()
-	} else if len(notifications) != 3 {
-		log.Error("TestStoreGetNotifications: Inserted 3 Notifications, Got ", len(notifications), ".")
+	}
+
+	if len(notifications) != len(Common.Notifications) {
+		log.Error("TestStoreGetNotifications: Got ", len(notifications), ".")
 		t.FailNow()
 	}
 
@@ -92,47 +95,19 @@ func TestStoreGetNotifications(t *testing.T) {
 }
 
 func TestStoreGetNotificationsByCheckID(t *testing.T) {
-	checkID := "00000"
-	log.Info("TestStoreGetNotificationsByCheckID: Getting Common.Notifications from store for CheckID", checkID)
-	if notifications, err := Common.DBStore.GetNotificationsByCheckID(Common.User, checkID); err != nil {
+	checkID := "00001"
+	log.Info("TestStoreGetNotificationsByCheckID: Getting Common.Notifications from store for CheckID ", checkID)
+
+	notifications, err := Common.DBStore.GetNotificationsByCheckID(Common.User, checkID)
+	if err != nil {
 		log.Error(err)
 		t.FailNow()
-	} else if len(notifications) != 2 {
-		log.Error("TestStoreGetNotificationsByCheckID: Deleted 3 Notifications and Expect 0, Got ", len(notifications), ".")
+	}
+	if len(notifications) != len(Common.Notifications) {
+		log.Error("TestStoreGetNotificationsByCheckID: Got ", len(notifications), ".")
 		t.FailNow()
 	}
 	log.Info("TestStoreGetNotificationsByCheckID: PASS.")
-}
-
-func TestStoreUpdateNotification(t *testing.T) {
-	checkID := "11111"
-	log.Info("TestStoreUpdateNotification: Getting Common.Notifications from store for CheckID", checkID)
-	notifications, err := Common.DBStore.GetNotifications(Common.User)
-	if err != nil {
-		log.Error(err)
-		t.FailNow()
-	}
-	for i, _ := range notifications {
-		log.Info("TestStoreUpdateNotification: Update notifications[", i, "] From Store (Set notifications[", i, "].CheckID to \"11111\").")
-		notifications[i].CheckID = checkID
-		if err := Common.DBStore.UpdateNotification(Common.User, notifications[i]); err != nil {
-			log.Error(err)
-			t.FailNow()
-		}
-	}
-	log.Info("TestStoreUpdateNotification: Validating Changes. Fetching Notifications.")
-	notifications, err = Common.DBStore.GetNotifications(Common.User)
-	if err != nil {
-		log.Error(err)
-		t.FailNow()
-	}
-	for i, _ := range notifications {
-		if notifications[i].CheckID != checkID {
-			log.Error(err)
-			t.FailNow()
-		}
-		log.Info("TestStoreUpdateNotification: notifications[", i, "].CheckID  was updated successfully.")
-	}
 }
 
 func TestStoreDeleteNotifications(t *testing.T) {
