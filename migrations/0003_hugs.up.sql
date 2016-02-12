7caacd34e499739deced93d95959bc31ff6956b8
Dan Compton <dan@opsee.com>
2016-02-12 13:50:06 -0500

    Fix.  Add new notification type.

--- migrations/0003_hugs.up.sql
+++ migrations/0003_hugs.up.sql
@@ -1,7 +1,3 @@
-DELETE FROM notification_types;
-INSERT INTO notification_types VALUES ('1', 'slack_bot');
-INSERT INTO notification_types VALUES ('2', 'slack_hook');
-INSERT INTO notification_types VALUES ('3', 'email');
 INSERT INTO notification_types VALUES ('4', 'web_hook');
 
 
