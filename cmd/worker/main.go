875a588ad0812620d679b9c585b030c0450d2e11
Mark Martin <assplecake@gmail.com>
2016-06-21 17:03:12 -0700

    block

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/opsee/hugs/config"
 	"github.com/opsee/hugs/consumer/nsq"
 	"github.com/opsee/hugs/util"
@@ -26,4 +30,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	<-sigChan
 }
